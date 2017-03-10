14ec38a9d09be1ed781b4d09ac2fcbe2e4bd0e41
ashwin <ashwinath@hotmail.com>
2017-03-10 20:04:36 +0800

    shortened description

--- data/portfolio.js
+++ data/portfolio.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
     title: 'This website',
-    picture: '/img/workstation.png',
-    description: 'This project was here to build my resume and reinforce my knowledge as a self taught developer who can learn about new technology. This website is using Node.js and Express.js. Although I do a lot of backend development at work, I try to be as all rounded as possible by trying some front end development and this is my first attempt at front end web development.',
+    picture: '/img/website.png',
+    description: 'This website is built using Node.js and Express.js. Although I do a lot of backend development at work, I try to be as all rounded as possible by trying some front end development and this is my first attempt at front end web development.',
     github: 'https://github.com/ashwinath/personal-site'
   },
   {
     title: 'Popular Movies Android App',
     picture: '/img/phone.png',
-    description: 'My first major project during my free time as a student. One of the biggest milestones I crossed since I was not formally trained as a computer scientist/developer. This app loads movies sorted according to popular, highest rated and upcoming movies. It allows you to watch trailers, read reviews and favourite the movie for later use. This was inspired by Udacity\'s Android Developer Nanodegree programme.',
+    description: 'My first major project during my free time as a student. This app loads movies sorted according to popular, highest rated and upcoming movies.',
     github: 'https://github.com/ashwinath/Android_Popular_Movies_App'
   }
 ]
