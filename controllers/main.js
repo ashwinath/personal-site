cb3d96998e341f9e0eb91c0ea8ee353f8a15d5a6
ashwin <ashwinath@hotmail.com>
2017-03-10 21:20:25 +0800

    updated work history

--- controllers/main.js
+++ controllers/main.js
@@ -1,14 +1,15 @@
 const express        = require('express'),
-      router         = express.Router(),
-      session        = require('express-session'),
       bodyParser     = require('body-parser'),
       flash          = require('connect-flash'),
       cookieParser   = require('cookie-parser'),
-      {cookieSecret} = require('../credentials/credentials'),
+      session        = require('express-session'),
       portfolio      = require('../data/portfolio'),
-      EmailManager   = require('../mediators/EmailManager');
+      workHistory    = require('../data/workHistory'),
+      EmailManager   = require('../mediators/EmailManager'),
+      {cookieSecret} = require('../credentials/credentials');
 
-const FLASH_SUCCESS         = 'success',
+const router                = express.Router(),
+      FLASH_SUCCESS         = 'success',
       FLASH_SUCCESS_MESSAGE = 'Your message has been successfully sent!',
       FLASH_FAILURE         = 'failure',
       FLASH_FAILURE_MESSAGE = 'Your message was not sent! Please send me an email at ashwinath@hotmail.com';
@@ -23,14 +24,24 @@ router.use(session({cookie: {maxAge: 60000}}))
 
 // MAIN PAGE
 router.get('/', (req, res) => {
-  res.render('mainPage/index', { portfolio: portfolio, success: req.flash(FLASH_SUCCESS), failure: req.flash(FLASH_FAILURE) });
+  res.render('mainPage/index', 
+    { 
+      portfolio: portfolio, 
+      success: req.flash(FLASH_SUCCESS), 
+      failure: req.flash(FLASH_FAILURE),
+      workHistoryList: workHistory
+    }
+  );
 });
 
 // CONTACT ME
 router.post('/contact', (req, res) => {
   var contactObject = req.body.contact;
   var subject = '[WEBSITE]' + contactObject.name + ' has contacted you!';
-  EmailManager.sendEmail(subject, contactObject, EmailManager.formatContactMe, (err, info) => {
+  EmailManager.sendEmail(subject,
+                         contactObject, 
+                         EmailManager.formatContactMe, 
+                         (err, info) => {
     if(err) {
       console.log('Error sending email: ' + err);
       req.flash(FLASH_FAILURE, FLASH_FAILURE_MESSAGE);
---
+++ data/workHistory.js
@@ -0,0 +1,6 @@
+module.exports = [
+  {
+    companyLogoSrc: '/img/Hewlett_Packard_Enterprise.png',
+    description: 'I worked at HPE as a junior developer writing backend Java enterprise applications. I write applications with Spring Framework, Spring Batch, Scriptella ETL and Quartz Scheduler. Other than development, I also provided L3 support where I have analysed logs, wrote tools to analyse logs better as well as investigating and solving problems faced in production.'
+  }
+];
