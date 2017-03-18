39b54f6a53f01dd66b098fc11aebeb9d41ca61c7
ashwin <ashwinath@hotmail.com>
2017-03-18 14:12:52 +0800

    added license and removed useless data

--- data/workHistory.js
+++ data/workHistory.js
@@ -1,20 +1,8 @@
 module.exports = [
   {
     companyLogoSrc: '/img/Hewlett_Packard_Enterprise.png',
-    companyName: 'Hewlett Packard Enterprise',
-    country: 'Singapore',
-    role: 'Java Developer',
-    start: 'July 2016',
-    end: 'Present',
-    description: 'I worked at HPE as a junior developer writing backend Java enterprise applications. I write applications with Spring Framework, Spring Batch, Scriptella ETL and Quartz Scheduler. Other than development, I also provided L3 support where I have analysed logs, wrote tools to analyse logs better as well as investigating and solving problems faced in production.'
   },
   {
     companyLogoSrc: '/img/epikk.png',
-    companyName: 'Epikk Co',
-    country: 'Singapore',
-    role: 'Content Marketer',
-    start: 'May 2015',
-    end: 'July 2015',
-    description: 'I interned at Epikk Co as a content marketer during my university days studying marketing. I helped with the publicity of the startup by doing copywriting and managing Google Adwords. We wrote articles on the company\'s blog and Medium of the various makers we met while we were at Epikk.'
   }
 ];
