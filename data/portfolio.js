7e39f77b27508432eb15f81e57edacac09d8efe2
ashwin <ashwinath@hotmail.com>
2017-03-18 11:04:07 +0800

    changed design for portfolio and work experience, changed section titles to uppercase

--- data/portfolio.js
+++ data/portfolio.js
@@ -2,13 +2,13 @@ module.exports = [
   {
     title: 'This website',
     picture: '/img/code.png',
-    description: 'This website is built using Node.js and Express.js. Although I do a lot of backend development at work, I try to be as all rounded as possible by trying some front end development and this is my first attempt at front end web development.',
+    description: 'Full Stack Development',
     github: 'https://github.com/ashwinath/personal-site'
   },
   {
-    title: 'Popular Movies Android App',
+    title: 'Popular Movies App',
     picture: '/img/phone.png',
-    description: 'My first major project during my free time as a student. This app loads movies sorted according to popular, highest rated and upcoming movies.',
+    description: 'Android Movies App',
     github: 'https://github.com/ashwinath/Android_Popular_Movies_App'
   }
 ]
